c3d55738a8c6270b2bf1a21604ad098d10ea2d5c
remeo08 <rmeorux5870@gmail.com>
2023-07-31 18:36:49 +0900

    diary editor

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import DiaryEditor from './DiaryEditor';
+
+function App() {
+    return (
+        <div className="App">
+            <DiaryEditor />
+        </div>
+    );
+}
+
+export default App;
